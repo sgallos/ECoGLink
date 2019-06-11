85104d8b81c6af676012d82511f673d613d0788d
Kevin Davis <kevincarrolldavis@gmail.com>
2019-06-10 23:50:30 -0400

    Module loader

---
+++ ECoGLink/__init__.py
@@ -0,0 +1,15 @@
+
+from enums import *
+
+class Application():
+
+    # Application-related variables
+    state = AppState.CONTINUOUS
+    # Device = Devices.Neomono
+
+    def __init__(self):
+
+
+
+if __name__ == '__main__':
+    print("Initializing ECoG Module")
