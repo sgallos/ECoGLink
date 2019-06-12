b5c703d1cd8261d716a10480071dbccaef658da4
Kevin Davis <kevincarrolldavis@gmail.com>
2019-06-12 09:30:35 -0400

    Added Device Base class. This is to ensure all devices classes have modes and processes

---
+++ ECoGLink/Devices/__init__.py
@@ -0,0 +1,24 @@
+
+from abc import ABC, abstractmethod, abstractproperty
+
+class Device(ABC):
+
+    def __init__(self):
+        return
+
+    @property
+    @abstractmethod
+    def modes(self):
+        pass
+
+    def process(self, signal_input):
+        mode = self.mode
+        mode.process(signal_input)
+
+    @property
+    def mode():
+        return self.__mode
+
+    @mode.setter
+    def mode(self, mode):
+        self.__mode = mode
