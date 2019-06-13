b93ccfe8aac9c4db0eaa8f2bd62391a2265d61ba
Charliealver <calver@med.miami.edu>
2019-06-13 15:40:05 -0400

    Create grab_data_pkts.py

---
+++ ECoGLink/Devices/grab_data_pkts.py
@@ -0,0 +1,12 @@
+"""
+Created on Thu Jun 13 13:28:45 2019
+
+
+Grab Data Packets
+"""
+
+# In Matlab is a function
+# inputs number of packets
+# for python will require existing in same inst. space as initialized location
+
+
