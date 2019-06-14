6401b62d9d8cb1cb60117274e167ec412e99c324
Kevin Davis <kevincarrolldavis@gmail.com>
2019-06-14 09:22:02 -0400

    Corrected Neomano spelling

--- ECoGLink/Devices/Neomano.py
+++ ECoGLink/Devices/Neomano.py
@@ -3,17 +3,17 @@ from enum import Enum
 from .Operation_mode import Operation_mode
 from .Nexus import NexusOutput
 
-class Neomono_state(Enum):
+class Neomano_state(Enum):
     STOPPED = 0
     FLEXED = 1
     EXTENDED = 2
 
-class Neomono_output(Enum):
+class Neomano_output(Enum):
     STOP = 0
     FLEX = 1
     EXTEND = 2
 
-class Neomono_mode(Enum):
+class Neomano_mode(Enum):
     CONTINUOUS = 1
     TOGGLE = 2
     TIMED = 3
@@ -35,28 +35,28 @@ class Continuous_Condition(Operation_mode):
     
     def process(self, BMI_input):
         if BMI_input == NexusOutput.MOVE:
-            Hardware_output = Neomono_output.FLEX
+            Hardware_output = Neomano_output.FLEX
         else:
-            Hardware_output = Neomono_output.EXTEND
+            Hardware_output = Neomano_output.EXTEND
         return Hardware_output
 
 #Toggle state
 class Toggle_Condition(Operation_mode):
     
-    def __init__(self, state = Neomono_state.STOPPED):
+    def __init__(self, state = Neomano_state.STOPPED):
         self.state = state
         return
     
     def process(self, BMI_input):
-        if BMI_input == NexusOutput.MOVE and self.state != Neomono_state.FLEXED:
-            Hardware_output = Neomono_output.FLEX
-            self.state = Neomono_state.FLEXED
-        elif BMI_input == NexusOutput.MOVE and self.state != Neomono_state.EXTENDED:
-            Hardware_output = Neomono_output.EXTEND
-            self.state = Neomono_state.EXTENDED
-        elif BMI_input == NexusOutput.REST and self.state == Neomono_state.FLEXED:
-            Hardware_output = Neomono_output.FLEX
+        if BMI_input == NexusOutput.MOVE and self.state != Neomano_state.FLEXED:
+            Hardware_output = Neomano_output.FLEX
+            self.state = Neomano_state.FLEXED
+        elif BMI_input == NexusOutput.MOVE and self.state != Neomano_state.EXTENDED:
+            Hardware_output = Neomano_output.EXTEND
+            self.state = Neomano_state.EXTENDED
+        elif BMI_input == NexusOutput.REST and self.state == Neomano_state.FLEXED:
+            Hardware_output = Neomano_output.FLEX
         else:
-            Hardware_output = Neomono_output.STOP
+            Hardware_output = Neomano_output.STOP
         
         return Hardware_output
--- tests/Devices/test_Neomano.py
+++ tests/Devices/test_Neomano.py
@@ -5,31 +5,31 @@ import pytest
 
 sys.path.append(os.path.realpath('./'))
 
-from ECoGLink.Devices.Neomono import *
+from ECoGLink.Devices.Neomano import *
 from ECoGLink.Devices.Nexus import *
 
 def test_toggle():
 
     # Should initialize to the state you set
-    toggle_mode = Toggle_Condition(state = Neomono_state.EXTENDED)
-    assert toggle_mode.state == Neomono_state.EXTENDED
+    toggle_mode = Toggle_Condition(state = Neomano_state.EXTENDED)
+    assert toggle_mode.state == Neomano_state.EXTENDED
 
     # Should change after processing a move
     toggle_mode.process(NexusOutput.MOVE)
-    assert toggle_mode.state != Neomono_state.EXTENDED
+    assert toggle_mode.state != Neomano_state.EXTENDED
 
     # Shouln't change after processing a rest
     toggle_mode.process(NexusOutput.REST)
-    assert toggle_mode.state != Neomono_state.EXTENDED
+    assert toggle_mode.state != Neomano_state.EXTENDED
 
     # Moving again should set it back
     toggle_mode.process(NexusOutput.MOVE)
-    assert toggle_mode.state == Neomono_state.EXTENDED
+    assert toggle_mode.state == Neomano_state.EXTENDED
 
 def test_continuous():
 
     continuous_mode = Continuous_Condition()
 
-    assert Neomono_output.FLEX == continuous_mode.process(NexusOutput.MOVE)
+    assert Neomano_output.FLEX == continuous_mode.process(NexusOutput.MOVE)
 
-    assert Neomono_output.EXTEND == continuous_mode.process(NexusOutput.REST)
+    assert Neomano_output.EXTEND == continuous_mode.process(NexusOutput.REST)
