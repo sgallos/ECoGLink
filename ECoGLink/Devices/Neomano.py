133b63e7bbed6efe0debc48f4157b82a3de9480d
Kevin Davis <kevincarrolldavis@gmail.com>
2019-06-14 09:35:40 -0400

    Renamed neomano state, mode, and output enumerations for clarity

--- ECoGLink/Devices/Neomano.py
+++ ECoGLink/Devices/Neomano.py
@@ -3,17 +3,17 @@ from enum import Enum
 from .Operation_mode import Operation_mode
 from .Nexus import NexusOutput
 
-class Neomano_state(Enum):
+class State(Enum):
     STOPPED = 0
     FLEXED = 1
     EXTENDED = 2
 
-class Neomano_output(Enum):
+class OutputCommand(Enum):
     STOP = 0
     FLEX = 1
     EXTEND = 2
 
-class Neomano_mode(Enum):
+class Mode(Enum):
     CONTINUOUS = 1
     TOGGLE = 2
     TIMED = 3
@@ -35,28 +35,28 @@ class Continuous_Condition(Operation_mode):
     
     def process(self, BMI_input):
         if BMI_input == NexusOutput.MOVE:
-            Hardware_output = Neomano_output.FLEX
+            Hardware_output = OutputCommand.FLEX
         else:
-            Hardware_output = Neomano_output.EXTEND
+            Hardware_output = OutputCommand.EXTEND
         return Hardware_output
 
 #Toggle state
 class Toggle_Condition(Operation_mode):
     
-    def __init__(self, state = Neomano_state.STOPPED):
+    def __init__(self, state = State.STOPPED):
         self.state = state
         return
     
     def process(self, BMI_input):
-        if BMI_input == NexusOutput.MOVE and self.state != Neomano_state.FLEXED:
-            Hardware_output = Neomano_output.FLEX
-            self.state = Neomano_state.FLEXED
-        elif BMI_input == NexusOutput.MOVE and self.state != Neomano_state.EXTENDED:
-            Hardware_output = Neomano_output.EXTEND
-            self.state = Neomano_state.EXTENDED
-        elif BMI_input == NexusOutput.REST and self.state == Neomano_state.FLEXED:
-            Hardware_output = Neomano_output.FLEX
+        if BMI_input == NexusOutput.MOVE and self.state != State.FLEXED:
+            Hardware_output = OutputCommand.FLEX
+            self.state = State.FLEXED
+        elif BMI_input == NexusOutput.MOVE and self.state != State.EXTENDED:
+            Hardware_output = OutputCommand.EXTEND
+            self.state = State.EXTENDED
+        elif BMI_input == NexusOutput.REST and self.state == State.FLEXED:
+            Hardware_output = OutputCommand.FLEX
         else:
-            Hardware_output = Neomano_output.STOP
+            Hardware_output = OutputCommand.STOP
         
         return Hardware_output
--- tests/Devices/test_Neomano.py
+++ tests/Devices/test_Neomano.py
@@ -5,31 +5,32 @@ import pytest
 
 sys.path.append(os.path.realpath('./'))
 
-from ECoGLink.Devices.Neomano import *
+import ECoGLink.Devices.Neomano as Neomano
+# from ECoGLink.Devices.Neomano import *
 from ECoGLink.Devices.Nexus import *
 
 def test_toggle():
 
     # Should initialize to the state you set
-    toggle_mode = Toggle_Condition(state = Neomano_state.EXTENDED)
-    assert toggle_mode.state == Neomano_state.EXTENDED
+    toggle_mode = Neomano.Toggle_Condition(state = Neomano.State.EXTENDED)
+    assert toggle_mode.state == Neomano.State.EXTENDED
 
     # Should change after processing a move
     toggle_mode.process(NexusOutput.MOVE)
-    assert toggle_mode.state != Neomano_state.EXTENDED
+    assert toggle_mode.state != Neomano.State.EXTENDED
 
     # Shouln't change after processing a rest
     toggle_mode.process(NexusOutput.REST)
-    assert toggle_mode.state != Neomano_state.EXTENDED
+    assert toggle_mode.state != Neomano.State.EXTENDED
 
     # Moving again should set it back
     toggle_mode.process(NexusOutput.MOVE)
-    assert toggle_mode.state == Neomano_state.EXTENDED
+    assert toggle_mode.state == Neomano.State.EXTENDED
 
 def test_continuous():
 
-    continuous_mode = Continuous_Condition()
+    continuous_mode = Neomano.Continuous_Condition()
 
-    assert Neomano_output.FLEX == continuous_mode.process(NexusOutput.MOVE)
+    assert Neomano.OutputCommand.FLEX == continuous_mode.process(NexusOutput.MOVE)
 
-    assert Neomano_output.EXTEND == continuous_mode.process(NexusOutput.REST)
+    assert Neomano.OutputCommand.EXTEND == continuous_mode.process(NexusOutput.REST)
