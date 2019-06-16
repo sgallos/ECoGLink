89d71b3d5cf8990bcd5b1b906aee1a6a36e3db35
Kevin Davis <kevincarrolldavis@gmail.com>
2019-06-16 00:26:28 -0400

    New Nexus restraints based on new java understanding

--- ECoGLink/Devices/Nexus/__init__.py
+++ ECoGLink/Devices/Nexus/__init__.py
@@ -7,12 +7,24 @@ class ClassifiedInput(Enum):
     REST = 0
 
 class State(Enum):
-    IDLE = 'IDLE'
-    LINKING_TO_INS = 'LINKING_TO_INS'
-    LINK_FAILED_NO_RESPONSE = 'LINK_FAILED_NO_RESPONSE'
-    LINK_FAILED_DEVICE_ERROR = 'LINK_FAILED_DEVICE_ERROR'
-    SUPERVISORY_SESSION = 'SUPERVISORY_SESSION'
-    MAINTENANCE_SESSION = 'MAINTENANCE_SESSION'
+    IDLE = 0
+    LINKING_TO_INS = 1
+    LINK_FAILED_NO_RESPONSE = 2
+    LINK_FAILED_DEVICE_ERR = 3
+    INS_CONNECTED = 4
+    MAINTENANCE_ENABLED = 5
+    ST_MDT_SESSION = 6
+
+class Status():
+    def __init__(self, state, majorVersion, minorVersion, batteryPercent,
+            batteryDepleted, hostTimeoutMinutes, maintenanceTimeoutSeconds):
+        self.state = state 
+        self.majorVersion = majorVersion 
+        self.minorVersion = minorVersion 
+        self.batteryPercent = batteryPercent 
+        self.batteryDepleted = batteryDepleted 
+        self.hostTimeoutMinutes = hostTimeoutMinutes 
+        self.maintenanceTimeoutSeconds = maintenanceTimeoutSeconds 
     
 class Port_Status(Enum):
     CONNECTED = 0.0
@@ -21,15 +33,36 @@ class Port_Status(Enum):
     ALREADY_OPENED = 4.0
     ALREADY_CONNECTED = -1.0
 
+class Response_Code(Enum):
+    SUCCESS = 0
+    PAYLOAD_CRC_ERROR = 1
+    INVALID_FRAME_TYPE = 2
+    MESSAGE_INCOMPLETE = 3
+    INVALID_FRAME_ID = 4
+    INVALID_PAYLOAD_LENGTH = 5
+    HEADER_CRC_ERROR = 6
+    PREV_CMD_BUSY = 7
+    INS_POR_INDICATED = 8
+    BATT_DEPLETED = 9
+    INVALID_DATA = 10
+    NO_RESPONSE = 11
+    NOT_CONNECTED = 12
+    NO_INS_CODE_AVAILABLE = -1
 
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jun 13 15:42:05 2019
 
-@author: charl
+@author: charl & Kevin
+@note: State is a avlue of the State enumeration. Status is a dictionary of
+values that includes state, battery life, etc. See Java files for more detail
 """
 
 class _Nexus(ABC):
+
+    is_initialized = False
+    port_status = Port_Status.NOT_FOUND
+
     
     @abstractmethod
     def __init__(self, port):
@@ -39,11 +72,25 @@ class _Nexus(ABC):
         pass
     
     @abstractmethod
-    def get_data_packet():
-        pass
-    
-    @abstractmethod
     def get_state():
         pass
     
+    @abstractmethod
+    def get_status():
+        pass
+
+    @abstractmethod
+    def set_configuration():
+        pass
+
+    @abstractmethod
+    def start_data_session():
+        pass
     
+    @abstractmethod
+    def get_data_packet():
+        pass
+
+    @abstractmethod
+    def stop_data_session():
+        pass
